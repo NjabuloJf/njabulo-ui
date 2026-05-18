36476b9827e26ee1e8c6b00e9095f11f6fe6432d
Njabulo Jb <fanajb65@gmail.com>
2026-05-18 13:21:01 +0200

    Update title in externalAdReply and add quoted message

--- plugins/alive.js
+++ plugins/alive.js
@@ -33,7 +33,7 @@ async (conn, mek, args, { from, sender, pushname, reply }) => {
               },
               forwardingScore: 999,
               externalAdReply: {
-                  title: "✨ ɴᴊᴀʙᴜʟᴏ ᴜɪ ✨",
+                  title: "ɴᴊᴀʙᴜʟᴏ ᴜɪ",
                   body: `👋 Hello ${userName}!`,
                   thumbnailUrl: config.FANAIMG,
                   sourceUrl: config.NJABULOURL,
@@ -41,7 +41,19 @@ async (conn, mek, args, { from, sender, pushname, reply }) => {
                   renderSmallThumbnail: true
               }
           }
-        });
+        }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: userName,
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${userName};USER;;;\nFN:${userName}\nitem1.TEL;waid=${sender.split('@')[0]}:${sender.split('@')[0]}\nitem1.X-ABLabel:User\nEND:VCARD`
+                }
+            }
+        } });
 
     } catch (e) {
         console.error("Alive Error:", e);
