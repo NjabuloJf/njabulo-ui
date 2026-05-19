56926b8faa9ca53bb3ed8c8119efb3807cb09a6f
Njabulo Jb <fanajb65@gmail.com>
2026-05-19 22:03:48 +0200

    Update audio message URL and mimetype in menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -86,11 +86,12 @@ cmd({
             }
         });
 
+
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/NjabuloJf/njabulo-data/raw/refs/heads/main/autovoice/menunew.m4a' },
-            mimetype: 'audio/mp4',
-            ptt: true
-        }, { quoted: mek });
+    audio: { url: 'https://github.com/NjabuloJf/njabulo-data/raw/main/audio/menu.mp3' },
+    mimetype: 'audio/mpeg',
+    ptt: true
+}, { quoted: mek });
         
 
         const messageID = sentMsg.key.id;
